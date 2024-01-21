e81a35c56e4467c465ae44c9d736af95c205e994
Carlos <carlosalonso9797@gmail.com>
2024-01-21 15:32:56 +0000

    ya esta hecho el main

--- src/main/java/software/ulpgc/visorimagenes/Main.java
+++ src/main/java/software/ulpgc/visorimagenes/Main.java
@@ -3,7 +3,7 @@ package software.ulpgc.visorimagenes;
 import javax.swing.*;
 import java.io.File;
 
-
+//visor de imagenes
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
