12e23fb5b7c06a72f67386a442cfa8cf4314787c
Carlos <carlosalonso9797@gmail.com>
2024-01-21 15:32:02 +0000

    ya esta hecho el nextimagecommand

---
+++ src/main/java/software/ulpgc/visorimagenes/NextImageCommand.java
@@ -0,0 +1,15 @@
+package software.ulpgc.visorimagenes;
+
+class NextImageCommand implements Command {
+    private final ImageDisplay imageDisplay;
+
+    public NextImageCommand(ImageDisplay imageDisplay) {
+        this.imageDisplay = imageDisplay;
+    }
+
+    @Override
+    public void execute() {
+        imageDisplay.show(imageDisplay.image().next());
+    }
+}
+
