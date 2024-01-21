e3918332404e6792ceb70d46d8dad39c9fcff781
Carlos <carlosalonso9797@gmail.com>
2024-01-21 15:32:43 +0000

    ya esta hecho el main

---
+++ src/main/java/software/ulpgc/visorimagenes/Main.java
@@ -0,0 +1,19 @@
+package software.ulpgc.visorimagenes;
+
+import javax.swing.*;
+import java.io.File;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            Image image = new FileImageLoader(new File("D:\\Users\\Carlos\\Desktop\\visor-de-imagenes\\imagenes")).load();
+            frame.imageDisplay().show(image);
+            frame.add("Anterior", new PreviousImageCommand(frame.imageDisplay()));
+            frame.add("Siguiente", new NextImageCommand(frame.imageDisplay()));
+            frame.setVisible(true);
+        });
+    }
+
+}
